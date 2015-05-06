6281200eb7413bcbd217aed26fca11deb68f6eec
chris-han <chrishan2000@hotmail.com>
2015-05-07 00:15:31 +0800

    add Mianzhi page

---
+++ WebUI2/Mianzhi.aspx
@@ -0,0 +1,217 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Mianzhi.aspx.cs" Inherits="WebUI2.Mianzhi" Async="true" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <%--  <meta name="viewport" content="width=device-width, initial-scale=1" />--%>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <title>Catch me if you can!</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- Custom CSS -->
+    <link href="css/grayscale.css" rel="stylesheet" />
+    <%--    <link href="css/jquery.mobile.structure-1.1.0.css" rel="stylesheet" />--%>
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <%--    <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">--%>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+    <form id="form1" runat="server">
+        <asp:HiddenField ID="width" runat="server" />
+        <asp:HiddenField ID="height" runat="server" />
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#ImageButton2">
+                        <i class="fa fa-play-circle"></i><span class="light">Start</span> Catch
+                    </a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+                    <ul class="nav navbar-nav navbar-right" style="float: right; margin-right: -15px">
+                        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#about">About</a>
+                        </li>
+
+                        <%--                    <li>
+                        <a class="page-scroll" href="#download">Download</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>--%>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>
+
+        <!-- Intro Header -->
+        <header class="intro">
+            <div class="intro-body">
+
+
+                <div class="container">
+
+
+                    <div class="row">
+
+                        <div class="col-lg-12">
+                            <h1 class="brand-heading">
+                                <asp:Label ID="lblCap" runat="server" Text="面值"></asp:Label></h1>
+                            <p class="intro-text">
+
+                                <asp:Label ID="lblRes" runat="server" class="intro-text">我会告诉你</asp:Label>
+                            </p>
+
+
+                        </div>
+
+
+                        <div class="col-lg-12">
+
+
+                            <asp:Label ID="lblMessage" runat="server" class="span12 intro-text">Pic1</asp:Label>
+                            <br />
+                            <asp:FileUpload ID="FileUpload1" runat="server" BorderStyle="None"/>
+                            <br />
+                            <asp:Image ID="Image1" runat="server" class="span12" Style="max-width: 70%" />
+
+                        </div>
+
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <asp:ImageButton ID="ImageButton2" runat="server" ImageUrl="~/img/catchme.png" OnClick="ImageButton2_Click" BorderStyle="None" CssClass="btn-circle" EnableTheming="False" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </header>
+
+        <!-- About Section -->
+        <section id="about" class="container content-section text-center">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>About Catch Me!</h2>
+                    <p>Based on Microsoft Machine Learning Project Oxford <a href="http://www.projectoxford.ai/" target="_blank">www.projectoxford.ai</a> for facial detection and recognization API</p>
+
+
+                </div>
+            </div>
+        </section>
+        <%-- 
+    <!-- Download Section -->
+    <section id="download" class="content-section text-center">
+        <div class="download-section">
+            <div class="container">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Download Grayscale</h2>
+                    <p>You can download Grayscale for free on the preview page at Start Bootstrap.</p>
+                    <a href="http://startbootstrap.com/template-overviews/grayscale/" class="btn btn-default btn-lg">Visit Download Page</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="container content-section text-center">fa fa-play-circle
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i><span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i><span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg"><i class="fa fa-google-plus fa-fw"></i><span class="network-name">Google+</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    <!-- Map Section -->
+    <div id="map"></div>
+        --%>
+        <!-- Footer -->
+        <footer>
+            <div class="container text-center">
+                <p>
+                    <a href="https://github.com/chris-han/FaceRecognization" target="_blank">
+                        <img src="img/github1.png" /></a>
+                </p>
+            </div>
+            <div class="container text-right">
+                <a class="page-scroll" href="#page-top">
+                    <i class="fa fa-long-arrow-up"></i><span class="light">Go</span> Up
+                </a>
+            </div>
+        </footer>
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js"></script>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                var width = $(window).width();
+                var height = $(window).height();
+
+                $("#width").val(width);
+                $("#height").val(height);
+
+            });
+        </script>
+
+        <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
+        <%--    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>--%>
+
+        <!-- Custom Theme JavaScript -->
+        <script src="js/grayscale.js"></script>
+    </form>
+</body>
+
+</html>
---
+++ WebUI2/Mianzhi.aspx.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Face.Controls;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Configuration;
+
+namespace WebUI2
+{
+    public partial class Mianzhi : System.Web.UI.Page
+    {
+        /// <summary>
+        /// FaceSDK subscription key
+        /// </summary>
+        private readonly string _subscriptionKey = ConfigurationManager.AppSettings["FaceAPIKey"].ToString();
+        private readonly string _AmlKey = ConfigurationManager.AppSettings["AMLAPIKey"].ToString();
+        private static FaceServiceClient _instance;
+        private double TheBrowserWidth;
+        private int DispWidth;
+        //private double TheBrowserHeight;
+
+        public static FaceServiceClient Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public int MaxImageSize
+        {
+            get
+            {
+                return 300;
+            }
+        }
+
+        private Dictionary<string, Microsoft.ProjectOxford.Face.Controls.Face> PhotoPair;
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                _instance = new FaceServiceClient(_subscriptionKey);
+            else
+            {
+                double.TryParse(width.Value, out TheBrowserWidth);
+                Trace.Write(string.Format("Browser width is {0}", TheBrowserWidth));
+                DispWidth = (int)(TheBrowserWidth / 2); //half screen
+                DispWidth = DispWidth > 400 ? 400 : (int)TheBrowserWidth;
+                //double.TryParse(height.Value, out TheBrowserHeight);
+            }
+        }
+
+        protected async void Catch()
+        {
+
+            //PhotoPair.Clear();
+            if (Session["PhotoPair"] == null)
+                PhotoPair = new Dictionary<string, Microsoft.ProjectOxford.Face.Controls.Face>();
+            else
+                PhotoPair = (Dictionary<string, Microsoft.ProjectOxford.Face.Controls.Face>)Session["PhotoPair"];
+
+            #region FirstFace
+            try
+            {
+                if (FileUpload1.HasFile)
+                {
+
+                    try
+                    {
+
+                        System.Drawing.Image image = System.Drawing.Image.FromStream(FileUpload1.FileContent);
+                      
+                        string FormetType = GetFileFormat(image);
+
+                        Byte[] btImage = GetImageBytes(image);
+
+
+                        string ImageUrl = GetImageURL(btImage);
+
+                        Image1.ImageUrl = ImageUrl;
+                        //Image1.Width = (int)DispWidth;
+                        double ratio = (double)DispWidth / image.Width;
+                        int DispHeight= (int)(image.Height * ratio);
+                        //Image1.Height = DispHeight;
+
+
+
+                        System.Drawing.Image SmallImage = btImage.Length > 4 * 1024* 1024 ? resizeImage(image, new Size(DispWidth, DispHeight)): image;
+                            
+                            //long jpegByteSize;
+
+                        Microsoft.ProjectOxford.Face.Contract.Face[] faces=null;
+                        using (MemoryStream stream = new MemoryStream())
+                        {
+                            SmallImage.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            stream.Seek(0, SeekOrigin.Begin);
+                            faces = await Instance.DetectAsync((Stream)stream, analyzesAge: false, analyzesGender: true);//must use the stream before other code here
+  
+                            stream.Close();                                                                                                                                                                                 // Handle REST API calling error
+                            //byteArray = stream.ToArray();
+                        }
+
+
+
+                        if (faces == null)
+                        {
+                            return;
+                        }
+                        var imageInfo = UIHelper.GetImageInfoForRendering(image);
+
+                        // Convert detection results into UI binding object for rendering
+                        foreach (var face in UIHelper.CalculateFaceRectangleForRendering(faces, MaxImageSize, imageInfo))
+                        {
+                            // Detected faces are hosted in result container, will be used in the verification later
+                            PhotoPair["Face1"] = face;
+                        }
+                    }
+                    catch (ClientException ex)
+                    {
+                        //Output = Output.AppendLine(string.Format("Response: {0}. {1}", ex.Error.Code, ex.Error.Message));
+                        return;
+                    }
+                }
+
+
+            }
+            catch (System.ArgumentException exp)
+            {
+                lblMessage.Text = "Invalid File";
+            }
+            catch (Exception ex)
+            {
+                lblMessage.Text = ex.Message;
+
+            }
+            #endregion
+
+
+            //Verification_Go();
+
+            //Session["PhotoPair"] = PhotoPair;
+        }
+
+        private static string GetFileFormat(System.Drawing.Image image)
+        {
+            string FormetType = string.Empty;
+            if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Tiff.Guid)
+                FormetType = "TIFF";
+            else if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Gif.Guid)
+                FormetType = "GIF";
+            else if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Jpeg.Guid)
+                FormetType = "JPG";
+            else if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Bmp.Guid)
+                FormetType = "BMP";
+            else if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Png.Guid)
+                FormetType = "PNG";
+            else if (image.RawFormat.Guid == System.Drawing.Imaging.ImageFormat.Icon.Guid)
+                FormetType = "ICO";
+            else
+                throw new System.ArgumentException("Invalid File Type");
+            return FormetType;
+        }
+
+        public byte[] GetImageBytes(System.Drawing.Image image)
+        {
+            byte[] byteArray = new byte[0];
+            using (MemoryStream stream = new MemoryStream())
+            {
+                image.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                stream.Close();
+
+                byteArray = stream.ToArray();
+            }
+            return byteArray;
+
+        }
+
+        //The actual converting function
+        public string GetImageURL(byte[] btImage)
+        {
+            return "data:image/jpg;base64," + Convert.ToBase64String(btImage);
+        }
+
+
+
+        private async void Verification_Go()
+        {
+            // Call face to face verification, verify REST API supports one face to one face verification only
+            // Here, we handle single face image only
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbCap = new StringBuilder();
+            if (PhotoPair.Count == 2)
+            {
+                //sb.AppendLine("Verifying...");
+                var faceId1 = PhotoPair["Face1"].FaceId;
+                var faceId2 = PhotoPair["Face2"].FaceId;
+                var gender1 = PhotoPair["Face1"].Gender;
+                var gender2 = PhotoPair["Face2"].Gender;
+
+                var genText = ((gender1 == null) || (gender2 == null)) ? "TA" : gender1 == "male" ? "他" : "她";
+
+                if ((gender1 != null) && (gender2 != null) && (gender1 != gender2))
+                {
+                    lblCap.Text = "逗比吗？";
+                    lblRes.Text = "泰国来的吧?";
+                    return;
+                }
+                //sb.AppendLine(string.Format("Request: Verifying face {0} and {1}", faceId1, faceId2));
+
+                // Call verify REST API with two face id
+                try
+                {
+
+                    var res = await Instance.VerifyAsync(Guid.Parse(faceId1), Guid.Parse(faceId2));
+                    double confid = res.Confidence * 100;
+                    // Verification result contains IsIdentical (true or false) and Confidence (in range 0.0 ~ 1.0),
+                    // here we update verify result on UI by VerifyResult binding
+                    //sb.AppendLine(string.Format("{0} ({1:0.0})", res.IsIdentical ? "Equals" : "Does not equal", res.Confidence));
+                    sbCap.AppendLine(string.Format("{0}{1}！", res.IsIdentical ? "是" : "不是", genText));
+                    sb.AppendLine(string.Format("向毛主席保证，我有{0}%把握", confid));
+                    // sb = string.Format("Response: Success. Face {0} and {1} {2} to the same person", faceId1, faceId2, res.IsIdentical ? "belong" : "not belong");
+                }
+                catch (ClientException ex)
+                {
+                    sb.AppendLine(string.Format("Response: {0}. {1}", ex.Error.Code, ex.Error.Message));
+                    return;
+                }
+            }
+            else
+            {
+                sb.AppendLine("Verification accepts two faces as input, please pick images with only one detectable face in it.");
+            }
+
+            lblCap.Text = sbCap.ToString();
+            lblRes.Text = sb.ToString();
+
+        }
+
+        protected async void ImageButton2_Click(object sender, ImageClickEventArgs e)
+        {
+            //Catch();
+            FaceVal x = await CallAmlService.CallAml();
+            lblCap.Text = x.ScoredClass.ToString();
+            Double p = x.Probability * 100;
+            lblRes.Text = string.Format("{0}%", Math.Round(p,2).ToString());
+        }
+
+        private static System.Drawing.Image resizeImage(System.Drawing.Image imgToResize, Size size)
+        {
+            return (System.Drawing.Image)(new Bitmap(imgToResize, size));
+        }
+
+    }
+}
